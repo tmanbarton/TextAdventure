c8fad3801b5684505f7dece806846a44b731fbd8
tmanbarton <tmanbarton@gmail.com>
2022-11-23 16:54:13 -0700

    add files for initial test

---
+++ textadventure/src/main/resources/static/getRequest.js
@@ -0,0 +1,40 @@
+$(document).ready(
+		function() {
+
+			// GET REQUEST
+			$("#getAllBooks").click(function(event) {
+				event.preventDefault();
+				ajaxGet();
+			});
+
+			// DO GET
+			function ajaxGet() {
+				$.ajax({
+					type : "GET",
+					url : "getBooks",
+					success : function(result) {
+						if (result.status == "success") {
+							$('#getResultDiv ul').empty();
+							$.each(result.data,
+									function(i, book) {
+										var user = "Book Name: "
+												+ book.bookName
+												+ ", Author: " + book.author
+												+ "<br>";
+										$('#getResultDiv .list-group').append(
+												user)
+									});
+
+							console.log("Success: ", result);
+						} else {
+							$("#getResultDiv").html("<strong>Error</strong>");
+							console.log("Fail: ", result);
+						}
+					},
+					error : function(e) {
+						$("#getResultDiv").html("<strong>Error</strong>");
+						console.log("ERROR: ", e);
+					}
+				});
+			}
+		})
