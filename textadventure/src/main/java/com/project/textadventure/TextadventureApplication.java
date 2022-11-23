3b5b2785c871fbbcc0590db7f88c66aad3dbf4c2
tmanbarton <tmanbarton@gmail.com>
2022-11-23 16:09:55 -0700

    initial commit

---
+++ textadventure/src/main/java/com/project/textadventure/TextadventureApplication.java
@@ -0,0 +1,13 @@
+package com.project.textadventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextadventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextadventureApplication.class, args);
+	}
+
+}
---
+++ textadventure/src/test/java/com/project/textadventure/TextadventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.textadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextadventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
