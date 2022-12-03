2d54856e227084395f85ce4fea900c4e6eeb655e
tmanbarton <tmanbarton@gmail.com>
2022-12-02 18:38:07 -0700

    add details to turn wheel action

--- textadventure/src/main/java/com/project/textadventure/game/Locations/Dam.java
+++ textadventure/src/main/java/com/project/textadventure/game/Locations/Dam.java
@@ -28,9 +28,29 @@ public class Dam extends Location {
         this.magnetDropped = magnetDropped;
     }
 
+    /**
+     * Connect the lake town location to the dam location.
+     * Change the description of all locations that reference the lake to not include the lake
+     * since there is no more lake.
+     */
     public void turnWheel() {
-        this.getConnectingLocations().get(4).setDirections(List.of("east", "e"));
-        this.getConnectingLocations().get(5).setDirections(List.of("down", "d"));
+        this.getConnectingLocations().get(3).setDirections(List.of("east", "e"));
+        this.getConnectingLocations().get(4).setDirections(List.of("down", "d"));
+
+        Location lake = this.getConnectingLocations().get(1).getLocation();
+        lake.setDescription("You are on the south side of an empty lake. There's a path going west and there's a dam to the north.");
+        lake.setShortDescription("You're on the south side of an empty lake.");
+
+        Location tailings = lake.getConnectingLocations().get(1).getLocation();
+        tailings.setDescription("All around are piles of tailings that look like they have been puked into this valley. There's not much else to be seen except the entrance to a mine to the south. There's a path leading east and another going north.");
+
+        Location intersection = tailings.getConnectingLocations().get(2).getLocation();
+        intersection.setDescription("You have reached an intersection in the road. It leads into the forest to the north and west and a southern road goes into a thinner part of the forest.");
+
+        Location driveway = intersection.getConnectingLocations().get(2).getLocation()
+                .getConnectingLocations().get(1).getLocation();
+        driveway.setDescription("You are at the west end of a dirt road surrounded by a forest of pine trees. There is a small gap to the north that exposes a steep, dirt driveway sloping down into the forest. Looking down the road to the east you can see over the trees and into the valley. There's also a foot path going northwest.");
+
         wheelTurned = true;
     }
 }
